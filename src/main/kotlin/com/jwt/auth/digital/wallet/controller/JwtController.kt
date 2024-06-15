2656a7c021665841f66bdf1e1a3ceb8e201fbe5e
rahulgubili <rahul.gubiligari@paypay-corp.co.jp>
2024-06-16 00:46:19 +0900

    Add JWT logic

---
+++ src/main/kotlin/com/jwt/auth/digital/wallet/controller/JwtController.kt
@@ -0,0 +1,31 @@
+package com.jwt.auth.digital.wallet.controller
+
+import com.jwt.auth.digital.wallet.dtos.response.OkResponse
+import com.jwt.auth.digital.wallet.exception.InvalidUsernameException
+import com.jwt.auth.digital.wallet.exception.UsernameAlreadyExistsException
+import com.jwt.auth.digital.wallet.service.CustomUserDetailsService
+import com.jwt.auth.digital.wallet.util.jwt.JwtUtil
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1")
+class JwtController(private val jwtUtil: JwtUtil, private val userDetailsService: CustomUserDetailsService) {
+
+    @PostMapping("/validate-jwt")
+    fun validateJwt(@RequestBody jwtToken: String): ResponseEntity<OkResponse> {
+
+        val username: String? = jwtUtil.extractUsername(jwtToken)
+        if (username == null) {
+            throw InvalidUsernameException("Username Invalid")
+        }
+        val userDetails = userDetailsService.loadUserByUsername(username)
+        val char = '"'
+        val trim = jwtToken.trim(char)
+        val result = jwtUtil.validateToken(trim, userDetails)
+        return ResponseEntity.ok(OkResponse(data = result))
+    }
+}
---
+++ src/main/kotlin/com/jwt/auth/digital/wallet/exception/InvalidUsernameException.kt
@@ -0,0 +1,3 @@
+package com.jwt.auth.digital.wallet.exception
+
+class InvalidUsernameException(message: String): RuntimeException(message)
